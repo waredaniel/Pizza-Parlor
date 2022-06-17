549b2e70fd8f095b7b6af6b808843d2cb214824f
danielware <waredanielb@gmail.com>
2022-06-17 14:35:35 -0700

    Add functioning form

--- js/scripts.js
+++ js/scripts.js
@@ -5,53 +5,71 @@ function Pizza(size, toppings) {
   this.toppings = toppings;
   this.sizePrice = 0;
   this.toppingsPrice = 0;
+  this.totalPrice = 0;
 
 }
 
 Pizza.prototype.SizePrice = function() {
-if (this.size === "small") {
+if (this.size === "Small") {
   this.sizePrice += 10;
 }
-if (this.size === "medium") {
+if (this.size === "Medium") {
   this.sizePrice += 15;
 }
-if (this.size === "large") {
+if (this.size === "Large") {
   this.sizePrice += 18;
 }
 return this.sizePrice;
 }
 
 Pizza.prototype.ToppingPrice = function() {
-  if (this.toppings.length === 1 ){
-    this.toppingsPrice += 2;
+  if (this.toppings.length === 0 ){
+    this.toppingsPrice += 0;
   };
-  if (this.toppings.length === 2 ) {
-   this.toppingsPrice += 4;
+  if (this.toppings.length === 1 ) {
+   this.toppingsPrice += 2;
   }
-  else {
+  if (this.toppings.length === 2) {
+    this.toppingsPrice +=4
+  }
+  if (this.toppings.length >= 3) {
     this.toppingsPrice += 6;
   }
   return this.toppingsPrice;
 }
 
 
-// Pizza.prototype.Cashier = function() {
-//   this.totalPrice = 0;
-//   this.totalPrice = this.totalPrice + this.sizePrice;
-//   return this.totalPrice;
+Pizza.prototype.Cashier = function() {
+  this.totalPrice = this.toppingsPrice + this.sizePrice;
+  return this.totalPrice; 
 
 
-// }
+}
 
 //UI Logic
 
-customersPizza = new Pizza ("medium", ["cheese", "ham"]);
-customersPizza.SizePrice();
-customersPizza.ToppingPrice();
+$(document).ready(function() {
+  $("form#customerorder").submit(function(event) {
+    event.preventDefault();
+    
+    
+    let toppingsChoice = [];
+    $("input:checkbox[name=toppings]:checked").each(function() {
+      toppingsChoice.push($(this).val());
+    });
+    let sizeChoice = $("input:radio[name=size]:checked").val();
+    
+    customersPizza = new Pizza (sizeChoice, toppingsChoice);
+    customersPizza.SizePrice();
+    customersPizza.ToppingPrice();
+  
 
 
-// customersPizza.Cashier();
-
+  $("#ordertotal").text("Your order comes to: $" + customersPizza.Cashier());
+});
+});
 
 console.log(customersPizza.sizePrice);
 console.log(customersPizza.toppingsPrice);
+console.log(customersPizza.totalPrice);
+
